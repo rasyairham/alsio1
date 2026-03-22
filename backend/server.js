72cb49929ed60943227ab111cf108877ccf5a47d
unknown <reyfanfadlan07@gmail.com>
2026-03-22 20:08:24 +0700

    up

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// ROUTES
 app.use('/api/auth', require('./routes/authRoutes'));
 
 // KONEKSI DATABASE
